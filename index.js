01c62718000dbcd26ce0c4660243de90c917df84
Niki <contact@zirobot.xyz>
2021-06-12 19:50:07 +0300

    if undefined

--- index.js
+++ index.js
@@ -5,32 +5,31 @@ const client = new discord.Client();
 client.on('ready', async () => {
   const logs = [
     `${client.user.username}`,
-    `Gif Channel => #${client.channels.cache.get(config.channels.gif).name} in ${client.channels.cache.get(config.channels.gif).guild.name}`,
-    `Icon Channel => #${client.channels.cache.get(config.channels.icon).name} in ${client.channels.cache.get(config.channels.icon).guild.name}`
+    `Gif Channel => #${client.channels.cache.get(config.channels.gif).name || 'No Channel'} in ${client.channels.cache.get(config.channels.gif).guild.name || 'No Server'}`,
+    `Icon Channel => #${client.channels.cache.get(config.channels.icon).name || 'No Channel'} in ${client.channels.cache.get(config.channels.icon).guild.name || 'No Server'}`
   ]
   console.log(logs.join('\n'))
 })
 
 client.on('userUpdate', async (oldu, newu) => {
-  try {
-    const oldav = oldu.displayAvatarURL({
-      dynamic: true
-    });
-    const newav = newu.displayAvatarURL({
-      dynamic: true
-    });
-    if (oldav != newav) {
-      let embed = new discord.MessageEmbed()
-        .setImage(newav)
-        .setFooter(newu.id)
-        .setColor('#000001')
-      if (newav.includes('.gif')) {
-        client.channels.cache.get(config.channels.gif).send(embed)
-      } else {
-        client.channels.cache.get(config.channels.icon).send(embed)
-      }
+  const oldav = oldu.displayAvatarURL({
+    dynamic: true
+  });
+  const newav = newu.displayAvatarURL({
+    dynamic: true
+  });
+  if (!newav) return;
+  if (oldav != newav) {
+    let embed = new discord.MessageEmbed()
+      .setImage(newav)
+      .setFooter(newu.id)
+      .setColor('#000001')
+    if (newav.includes('.gif')) {
+      client.channels.cache.get(config.channels.gif).send(embed).catch(() => {})
+    } else {
+      client.channels.cache.get(config.channels.icon).send(embed).catch(() => {})
     }
-  } catch (e) {}
+  }
 })
 
 
